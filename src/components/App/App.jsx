6460fdb832a24a8524b177575cb36e09a7a836df
joshi07saurabh <joshi07saurabh@users.noreply.github.com>
2022-02-12 22:12:29 +0530

    make speed fast

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,7 @@ import Landing from '../Landing/Landing';
 import Footer from '../Footer/Footer';
 import './App.css'
 import ChallengeSection from '../ChallengeSection/ChallengeSection';
-import { SAMPLE_PARAGRAPHS } from "../../data/sampleParagraphs";
+import { SAMPLE_PARAGRAPHS } from "../../data/sample.js";
 const time=60
 const url='http://metaphorpsum.com/paragraphs/1/9'
 const defaultState={ 
